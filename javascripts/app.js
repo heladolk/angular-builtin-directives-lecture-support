2635316f6792ed4456485daf78e7a6c44f547b28
heladolk <heladolk@yahoo.com>
2016-08-24 15:12:01 -0700

    add solutions

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,11 +1,12 @@
 angular.module('myApp',[]).controller('myController',function($scope){
-  $scope.fruits = ['apple', 'banana', 'peach'];
-  $scope.show = true;
-  $scope.add = function(fruit) {
-      $scope.fruits.push(fruit);
-      $scope.fruit = "";
-  };
-  $scope.toggle = function(){
-      $scope.show = !($scope.show);
-  };
+    $scope.view = {};
+    $scope.fruits = ['apple', 'banana', 'peach'];
+    $scope.view.show = true;
+    $scope.add = function(fruit) {
+        $scope.fruits.push(fruit);
+        $scope.fruit = "";
+    };
+    $scope.toggle = function(){
+        $scope.view.show = !($scope.view.show);
+    };
 });
