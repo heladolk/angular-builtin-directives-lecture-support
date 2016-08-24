5165308226528a5c4074d744793a6194cadb11f3
heladolk <heladolk@yahoo.com>
2016-08-24 14:33:31 -0700

    add solutions for ngmodel and ngshow

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,3 +1,11 @@
 angular.module('myApp',[]).controller('myController',function($scope){
-  $scope.fruit = 'default apple'
-})
+  $scope.fruits = ['apple', 'banana', 'peach'];
+  $scope.show = true;
+  $scope.add = function(fruit) {
+      $scope.fruits.push(fruit);
+      $scope.fruit = "";
+  };
+  $scope.toggle = function(){
+      $scope.show = !($scope.show);
+  };
+});
